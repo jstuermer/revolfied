4b49c35f8c8afc1d6fdb1a854d09604314b70186
Julian Stürmer <18049549+jstuermer@users.noreply.github.com>
2024-03-10 13:21:45 +0100

    Add Player component
    Add player movement system

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
 
+mod components;
+
 mod systems;
-use systems::spawn_player;
+use systems::*;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, player_movement);
     }
 }
--- src/game/player/systems.rs
+++ src/game/player/systems.rs
@@ -1,5 +1,10 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
+use super::components::Player;
+
+const PLAYER_SPEED: f32 = 500.0;
+
+/// Spawn the player sprite.
 pub fn spawn_player(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -7,9 +12,37 @@ pub fn spawn_player(
 ) {
     let window: &Window = window_query.get_single().unwrap();
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        texture: asset_server.load("sprites/spacecraft_player.png"),
-        ..default()
-    });
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/spacecraft_player.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let mut player_transform = player_query.single_mut();
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction += Vec3::new(-1.0, 0.0, 0.0)
+    }
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction += Vec3::new(1.0, 0.0, 0.0)
+    }
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
+        direction += Vec3::new(0.0, -1.0, 0.0)
+    }
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        direction += Vec3::new(0.0, 1.0, 0.0)
+    }
+
+    direction = direction.normalize_or_zero();
+    player_transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
 }
