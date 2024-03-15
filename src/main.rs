a18de802092d038ce8eff4a7b3651f8e824578c8
Julian Stürmer <18049549+jstuermer@users.noreply.github.com>
2024-03-15 18:51:04 +0100

    Set window to borderless fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 
 mod camera;
 use camera::spawn_camera;
@@ -8,7 +8,14 @@ use game::GamePlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                mode: WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(Time::<Fixed>::from_hz(200.0))
         .add_plugins(GamePlugin)
         .add_systems(Startup, spawn_camera)
